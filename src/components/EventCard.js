38b8600c05cc1610d8b3c714007c52562cd48b6b
eewilson32 <eewilson32@gmail.com>
2024-09-29 12:36:55 -0400

    Updated EventCard to display description and category instead of pricing

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -9,13 +9,12 @@ function EventCard({ event }) {
         <div className="event-card event-ticket" key={index}>
           <h3 className="event-title">{event.eventName}</h3>
           <p>{detail.date} - {detail.time}</p>
-          <div className="event-pricing">
-            <p>Box: ${detail.ticketPrices.box}</p>
-            <p>Orchestra: ${detail.ticketPrices.orchestra}</p>
-            <p>Main Floor: ${detail.ticketPrices.mainFloor}</p>
-            <p>Balcony: ${detail.ticketPrices.balcony}</p>
+          <div className="event-description">
+            <p>{event.description}</p>
           </div>
-          {/* Use Link to route to event details page */}
+          <div className="event-category">
+            <p>Category: {event.category}</p>
+          </div>
           <Link to={`/event/${event.eventName.replace(/\s+/g, '-').toLowerCase()}/${detail.date}`}>
             <button className="details-button">View Details</button>
           </Link>
