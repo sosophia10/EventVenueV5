788d98865ffdbfda5f75933f90727a1e4e90122b
eewilson32 <eewilson32@gmail.com>
2024-10-01 09:33:06 -0400

    Reverting Tickets.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -13,8 +13,6 @@ function Cart() {
       const parsedCart = JSON.parse(cartData);
       setCart(parsedCart);
 
-      
-
       // Calculate total price
       const ticketPrices = {
         box: 300.00,
@@ -31,37 +29,6 @@ function Cart() {
       setTotalPrice(total);
     }
   }, []);
-
-
-   // Function to calculate the total price
-   const calculateTotalPrice = (updatedCart) => {
-
-    // Calculate total price
-    const ticketPrices = {
-      box: 300.00,
-      orchestra: 200.00,
-      mainFloor: 150.00,
-      balcony: 100.00
-    };
-
-    
-    const total = (parsedCart.box * ticketPrices.box) +
-                  (parsedCart.orchestra * ticketPrices.orchestra) +
-                  (parsedCart.mainFloor * ticketPrices.mainFloor) +
-                  (parsedCart.balcony * ticketPrices.balcony);
-    setTotalPrice(total);
-  };
-
-  // Function to handle changes in ticket quantities
-  const handleTicketChange = (type, value) => {
-    const updatedCart = {
-      ...cart,
-      [type]: Number(value),
-    };
-    setCart(updatedCart);
-    calculateTotalPrice(updatedCart);  // Recalculate the total price
-    localStorage.setItem('cart', JSON.stringify(updatedCart));  // Update localStorage
-  };
 
   return (
     <div className="cart-page">
